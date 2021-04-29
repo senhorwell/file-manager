e512ec0834738737b1b015e1a48fce7982590cbb
Anderson Makino <anddersonful@gmail.com>
2021-04-29 17:46:21 -0300

    removing not needed lines

--- crud-file-server.js
+++ crud-file-server.js
@@ -1,28 +1,17 @@
 var fs = require('fs');
 
-// don't let users crawl up the folder structure by using a/../../../c/d
 var cleanUrl = function(url) { 
 	url = decodeURIComponent(url);
 	while(url.indexOf('..') >= 0) { url = url.replace('..', ''); }
 	return url;
 };
 
-/*  
-example usage:
-	require('http').createServer(function (req, res) {
-		server.handleRequest(port, path, req, res, vpath);
-	}).listen(port);
-*/
 exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequests) {	
-	// vpath: (optional) virtual path to host in the url
-	// path: the file system path to serve
-	// readOnly: whether to allow modifications to the file
 
-	// our error handler
 	var writeError = function (err, code) { 
 		code = code || 500;
 		console.log('Error ' + code + ': ' + err);
-		// write the error to the response, if possible
+
 		try {			
 			res.statusCode = code;
 			res.setHeader('Content-Type', 'application/json');
@@ -32,16 +21,14 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 		}
 	};
 
-	if(path.lastIndexOf('/') !== path.length - 1) { path += '/'; } // make sure path ends with a slash	
+	if(path.lastIndexOf('/') !== path.length - 1) { path += '/'; }	
 	var parsedUrl = require('url').parse(req.url);	
 	var query = query ? {} : require('querystring').parse(parsedUrl.query);
     var url = cleanUrl(parsedUrl.pathname);
 	
-	// normalize the url such that there is no trailing or leading slash /
 	if(url.lastIndexOf('/') === url.length - 1) { url = url.slice(0, url.length ); }
 	if(url[0] === '/') { url = url.slice(1, url.length);  }
 
-	// check that url begins with vpath
 	if(vpath && url.indexOf(vpath) != 0) {
 		console.log('url does not begin with vpath');
 		throw 'url [' + url + '] does not begin with vpath [' + vpath + ']';
@@ -63,7 +50,7 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 					if(logHeadRequests) {
 						console.log('head: ' + relativePath);				
 					}
-					fs.stat(relativePath, function(err, stats) { // determine if the resource is a file or directory
+					fs.stat(relativePath, function(err, stats) {
 						if(err) { writeError(err); } 
 						else {					
 							res.setHeader('Last-Modified', stats.mtime);		
@@ -88,12 +75,12 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 						}
 					});
 					break;
-				case 'GET': // returns file or directory contents
+				case 'GET':
 					console.log('relativePath: ' + relativePath);
 					if(url === 'favicon.ico') { 	
-						res.end(); // if the browser requests favicon, just return an empty response
+						res.end();
 					} else {
-						fs.stat(relativePath, function(err, stats) { // determine if the resource is a file or directory
+						fs.stat(relativePath, function(err, stats) {
 							if(err) { writeError(err); } 
 							else {
 								if(stats.isDirectory()) {
@@ -102,7 +89,6 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 									res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0");
 									res.setHeader("Cache-Control", "post-check=0, pre-check=0");
 									res.setHeader("Pragma", "no-cache");
-									// if it's a directory, return the files as a JSONified array
 									console.log('reading directory ' + relativePath);
 									fs.readdir(relativePath, function(err, files) {
 										if(err) { 
@@ -152,7 +138,6 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 										}
 									});
 								} else {
-									// if it's a file, return the contents of a file with the correct content type
 									console.log('reading file ' + relativePath);
 									if(query.type == 'json' || query.dir == 'json') {
 										var type = 'application/json';
@@ -182,11 +167,11 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 						});
 					}
 					return;
-				case 'PUT': // write a file
+				case 'PUT':
 					console.log('writing ' + relativePath);
 					var stream = fs.createWriteStream(relativePath);		
 					stream.ok = true;
-					req.pipe(stream); // TODO: limit data length
+					req.pipe(stream);
 					stream.on('close', function() { 
 						if(stream.ok) {
 							res.end();
@@ -197,12 +182,10 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 						writeError(err);
 					});
 					return;
-				case 'POST': // create a directory or rename a file or directory
-					if(query.rename) { // rename a file or directory
+				case 'POST':
+					if(query.rename) {
 						console.log('rename: ' + relativePath);
-						// e.g., http://localhost/old-name.html?rename=new-name.html
 						query.rename = cleanUrl(query.rename);
-						// TODO: handle missing vpath here
 						if(vpath) { 
 							if(query.rename.indexOf('/' + vpath + '/') == 0) { 
 								query.rename = query.rename.slice(vpath.length + 2, query.rename.length);
@@ -217,8 +200,7 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 								res.end();
 							}
 						});
-					} else if(query.create == 'directory') { // rename a directory
-						// e.g., http://localhost/new-directory?create=directory
+					} else if(query.create == 'directory') {
 						console.log('creating directory ' + relativePath);
 						fs.mkdir(relativePath, 0777, function(err) { 
 							if(err) { writeError(err); } 
@@ -231,11 +213,11 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 						writeError('valid queries are ' + url + '?rename=[new name] or ' + url + '?create=directory');
 					}
 					return;
-				case 'DELETE': // delete a file or directory				
+				case 'DELETE':			
 					fs.stat(relativePath, function(err, stats) { 
 						if(err) { writeError(err); } 
 						else {
-							if(stats.isDirectory()) { // delete a directory
+							if(stats.isDirectory()) {
 								console.log('deleting directory ' + relativePath);
 								fs.rmdir(relativePath, function(err) {
 									if(err) { writeError(err); }
@@ -243,7 +225,7 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 										res.end(); 
 									}
 								});
-							} else { // delete a file
+							} else {
 								console.log('deleting file ' + relativePath);
 								fs.unlink(relativePath, function(err) {
 									if(err) { writeError(err); }
@@ -255,17 +237,13 @@ exports.handleRequest = function(vpath, path, req, res, readOnly, logHeadRequest
 						}
 					});			
 					return;
-				default: // unsupported method! tell the client ...
+				default:
 					console.log('unsupported: ' + relativePath);				
 					writeError('Method ' + method + ' not allowed', 405);
 					return;
 			}
 		}
 	} catch(err) { 
-		// file system ('fs') errors are just bubbled up to this error handler
-		// for example, if the GET is called on a non-existent file, an error will be thrown
-		// and caught here
-		// writeError will write the error information to the response
 		writeError('unhandled error: ' + err);
 	}
 };
